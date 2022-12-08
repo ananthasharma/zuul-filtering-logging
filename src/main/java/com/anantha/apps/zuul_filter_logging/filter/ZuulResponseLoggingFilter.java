efafa274d06102d0b5740a2133c30e305d375cef
Anantha Sharma <Anantha.Sharma4@gmail.com>
2022-12-08 21:12:22 +0000

    removed unused imports

--- src/main/java/com/anantha/apps/zuul_filter_logging/filter/ZuulResponseLoggingFilter.java
+++ src/main/java/com/anantha/apps/zuul_filter_logging/filter/ZuulResponseLoggingFilter.java
@@ -1,19 +1,15 @@
 package com.anantha.apps.zuul_filter_logging.filter;
 
-import com.anantha.apps.zuul_filter_logging.constants.Constants;
 import com.anantha.apps.zuul_filter_logging.constants.Utils;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
-import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang.time.DurationFormatUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author Anantha Sharma
@@ -43,7 +39,7 @@ public class ZuulResponseLoggingFilter extends ZuulFilter {
         var callFor = RequestContext.getCurrentContext().getRequest().getRequestURI();
         long startMillis = Utils.getStartTimeFromRequestHeader();
         long endMillis = System.currentTimeMillis();
-        var duration = endMillis-startMillis;
+        var duration = endMillis - startMillis;
         var durationAsString = DurationFormatUtils.formatDuration(duration, "HH:mm:ss.SSS");
         LOG.info("response for: {}, duration(HH:mm:ss.SSS): {}", callFor, durationAsString);
         return null;
